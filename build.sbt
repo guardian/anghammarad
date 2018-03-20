9ff6dda96f5baf2024b489b328a1a9257c6febb9
Adam Fisher <adam.fisher@guardian.co.uk>
2018-03-20 13:38:47 +0000

    Stub Lambda and add packaging support for deployment

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := "anghammarad"
+name         := "anghammarad"
 organization := "com.gu"
 scalaVersion := "2.12.4"
 version      := "0.1.0-SNAPSHOT"
@@ -9,9 +9,20 @@ libraryDependencies ++= Seq(
   "com.amazonaws" % "aws-lambda-java-events" % "1.3.0",
   "com.amazonaws" % "aws-lambda-java-core" % "1.1.0",
   "com.amazonaws" % "aws-java-sdk-lambda" % awsSdkVersion,
+  "com.vladsch.flexmark" % "flexmark-all" % "0.32.18",
   "com.typesafe.scala-logging" %% "scala-logging" % "3.8.0",
   "ch.qos.logback" %  "logback-classic" % "1.1.7",
   "org.scalatest" %% "scalatest" % "3.0.5" % Test
 )
 
-scalacOptions := Seq("-unchecked", "-deprecation")
+scalacOptions ++= Seq(
+  "-deprecation",
+  "-Xfatal-warnings",
+  "-encoding", "UTF-8",
+  "-target:jvm-1.8",
+  "-Ywarn-dead-code"
+)
+
+enablePlugins(JavaAppPackaging)
+topLevelDirectory in Universal := None
+packageName in Universal := normalizedName.value
--- src/main/scala/com/gu/anghammarad/Main.scala
+++ src/main/scala/com/gu/anghammarad/Main.scala
@@ -1,11 +1,26 @@
 package com.gu.anghammarad
 
-
 import com.amazonaws.services.lambda.runtime.events.SNSEvent
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
+import collection.JavaConverters._
+
 
 class Main extends RequestHandler[SNSEvent, Unit] {
   override def handleRequest(input: SNSEvent, context: Context): Unit = {
+    val msgs = input.getRecords.asScala.map { snsRecord =>
+      val msgId = snsRecord.getSNS.getMessageId
 
+      val msgType = snsRecord.getSNS.getType
+      val source = snsRecord.getEventSource
+      val timestamp = snsRecord.getSNS.getTimestamp
+
+      val subject = snsRecord.getSNS.getSubject
+      val message = snsRecord.getSNS.getMessage
+      val msgAttrs = snsRecord.getSNS.getMessageAttributes.asScala.map { case (key, msgAttr) =>
+        s"$key [${msgAttr.getType}]: ${msgAttr.getValue}"
+      }.mkString("{", ",", "}")
+      s"$msgId ($msgType $source $timestamp): [$subject] $message $msgAttrs"
+    }
+    context.getLogger.log(msgs.mkString("{", ",", "}"))
   }
 }

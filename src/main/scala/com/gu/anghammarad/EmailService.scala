70c3e4530c74f5f1184dd1367478f6e7f4ce8a91
Jacob Winch <jacob.winch@guardian.co.uk>
2018-03-21 14:22:55 +0000

    Add Email Service

---
+++ src/main/scala/com/gu/anghammarad/EmailService.scala
@@ -0,0 +1,43 @@
+package com.gu.anghammarad
+
+import com.amazonaws.auth.{AWSCredentialsProviderChain, EnvironmentVariableCredentialsProvider}
+import com.amazonaws.auth.profile.ProfileCredentialsProvider
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.simpleemail.model.{Body, Content, Destination, SendEmailRequest, SendEmailResult, Message => AwsMessage}
+import com.amazonaws.services.simpleemail.{AmazonSimpleEmailService, AmazonSimpleEmailServiceClientBuilder}
+import scala.util.Try
+
+object EmailService {
+
+  private val provider = new AWSCredentialsProviderChain(
+    new ProfileCredentialsProvider("deployTools"),
+    new EnvironmentVariableCredentialsProvider()
+  )
+
+  val client: AmazonSimpleEmailService = AmazonSimpleEmailServiceClientBuilder.standard().withRegion(Regions.EU_WEST_1)
+    .withCredentials(provider)
+    .build()
+
+  def emailRequest(recipientAddress: String, message: models.Message): SendEmailRequest = {
+
+    def buildContent(data: String) = new Content().withCharset("UTF-8").withData(data)
+
+    val awsMessage = new AwsMessage()
+      .withSubject(buildContent(message.subject))
+      .withBody(new Body().withHtml(buildContent(message.contents)))
+
+    new SendEmailRequest()
+      .withDestination(new Destination().withToAddresses(recipientAddress))
+      .withSource("dig.dev.tooling@theguardian.com")
+      .withMessage(awsMessage)
+
+  }
+
+  def sendEmail(recipientAddress: String, message: models.Message): Try[SendEmailResult] = {
+    Try(client.sendEmail(emailRequest(recipientAddress, message)))
+  }
+
+}
+
+
+

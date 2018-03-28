c5ed2d1c96b76df1863440a7da8959ecfa2b95ec
Adam Fisher <adam.fisher@guardian.co.uk>
2018-03-28 17:51:02 +0100

    Single function with a default argument is probably ok

--- client/src/main/scala/com/gu/anghammarad/Anghammarad.scala
+++ client/src/main/scala/com/gu/anghammarad/Anghammarad.scala
@@ -25,33 +25,13 @@ object Anghammarad {
     */
   def notify(subject: String, message: String, sourceSystem: String,
              channel: RequestedChannel, target: List[Target], actions: List[Action],
-             topicArn: String, client: AmazonSNSAsync)
+             topicArn: String,
+             client: AmazonSNSAsync = AWS.snsClient(AWS.credentialsProvider()))
             (implicit executionContext: ExecutionContext): Future[String] = {
     val request = new PublishRequest()
       .withTopicArn(topicArn)
       .withSubject(subject)
       .withMessage(messageJson(message, sourceSystem, channel, target, actions))
     awsToScala(client.publishAsync)(request).map(_.getMessageId)
-  }
-
-  /**
-    * Use a default SNS client to create a notification.
-    *
-    * @param subject      Used for the subject in emails and the heading of hangouts chat notifications
-    * @param message      The message body. Supports markdown, but support differs between notification channels
-    * @param sourceSystem The system sending the notification (your system)
-    * @param channel      The notification channel you'd like to use
-    * @param target       Specify who should receive the message
-    * @param actions      Specify Call To Action buttons that will be put at the end of an email / hangout message
-    * @param topicArn     ARN of Anghammarad's SNS topic (you will need to obtain this and put it in your app's config)
-    * @return             Future of the resulting SNS Message ID
-    */
-  def notify(subject: String, message: String, sourceSystem: String,
-             channel: RequestedChannel, target: List[Target], actions: List[Action],
-             topicArn: String)
-            (implicit executionContext: ExecutionContext): Future[String] = {
-    val credentialsProvider = AWS.credentialsProvider()
-    val client = AWS.snsClient(credentialsProvider)
-    notify(subject, message, sourceSystem, channel, target, actions, topicArn, client)
   }
 }

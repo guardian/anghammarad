3d2339e2077cbc1b26c236e22363d0cf17cc17f3
Michael Mcnamara <michael.mcnamara@theguardian.com>
2021-11-16 11:03:18 +0000

    convert Hangouts service to use sttp instead of lolhttp

--- anghammarad/src/main/scala/com/gu/anghammarad/messages/HangoutsService.scala
+++ anghammarad/src/main/scala/com/gu/anghammarad/messages/HangoutsService.scala
@@ -2,30 +2,31 @@ package com.gu.anghammarad.messages
 
 import com.gu.anghammarad.AnghammaradException.Fail
 import com.gu.anghammarad.models.HangoutMessage
-import lol.http.{Client, Post}
 
-import scala.concurrent.duration._
 import scala.util.{Success, Try}
-
+import sttp.client3._
 
 object HangoutsService {
-  // using a blocking call, but lolhttp still requires an execution context
-  import scala.concurrent.ExecutionContext.Implicits.global
 
-  val client = Client("chat.googleapis.com", 443, "https")
+  def postMessage(webhook: String, message: String): Response[Either[String, String]] = {
+    val backend = HttpURLConnectionBackend()
+    basicRequest
+      .body(message)
+      .post(uri"$webhook")
+      .send(backend)
+  }
+
+  def checkResponse(response: Response[Either[String,String]]): Try[Unit] = {
+    if (response.isSuccess)
+      Success(())
+    else
+      Fail(s"Unable to send message. Received response code of: ${response.statusText}.")
+  }
 
   def sendHangoutsMessage(webhook: String, message: HangoutMessage): Try[Unit] = {
-    val request = Post(webhook.stripPrefix("https://chat.googleapis.com"), message.cardJson)
-    val (status, response) = client.runSync(request, timeout = 5.seconds) { response =>
-      val status = response.status
-      for {
-        response <- response.readAs[String]
-      } yield (status, response)
-    }
-    if (status == 200) {
-      Success(())
-    } else {
-      Fail(s"Got $status from chat.googleapis.com, could not send message: $response")
-    }
+    for {
+      response <- Try {postMessage(webhook, message.cardJson)}
+      successOrFailure <- checkResponse(response)
+    } yield successOrFailure
   }
 }
